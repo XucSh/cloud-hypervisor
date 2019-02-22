395f0051f3e2c48654796a1d7cbe319639f59682
Samuel Ortiz <sameo@linux.intel.com>
2019-02-22 17:05:02 +0100

    cloud-hypervisor: Call into the test_vm() routine
    test_vm is a dummy VM workload, we use it to test our initial VMM
    settings.
    
    Signed-off-by: Samuel Ortiz <sameo@linux.intel.com>

--- vmm/src/lib.rs
+++ vmm/src/lib.rs
@@ -16,11 +16,11 @@ pub fn test_vm() {
     let code = [
         0xba, 0xf8, 0x03, /* mov $0x3f8, %dx */
         0x00, 0xd8, /* add %bl, %al */
-        0x04, '0' as u8, /* add $'0', %al */
-        0xee,      /* out %al, (%dx) */
-        0xb0, '\n' as u8, /* mov $'\n', %al */
-        0xee,       /* out %al, (%dx) */
-        0xf4,       /* hlt */
+        0x04, b'0', /* add $'0', %al */
+        0xee, /* out %al, (%dx) */
+        0xb0, b'\n', /* mov $'\n', %al */
+        0xee,  /* out %al, (%dx) */
+        0xf4,  /* hlt */
     ];
 
     let mem_size = 0x1000; 
